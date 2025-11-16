865c6fc72ef74b62cf15b38a2a984eeae49ba0f3
manu <kaurmanmeen67@gmail.com>
2025-11-16 20:10:29 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,25 @@
+//the first alert
+alert("hello world of chirkutts");
+//this is how to do a line comment
+/*
+this is how to do a comment in general or multiple line comments
+*/
+//the second alert
+alert("how are you");
+//make the image invisible
+//move the image from top to bottom
+//make the image visible *changing the order of these three will effect the fuctionality too or we can say that js mves from top to bottom
+//js variables
+console.log("hello guys")
+console.log("hello world") 
+//boolean that is true or false 
+//if statements
+var ulikemeat = false;
+
+if (ulikemeat){
+
+    document.writeln("here is the menu")
+}
+
+
+
